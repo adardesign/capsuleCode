4f1faba2285bdaf58dcc4f73b9803117c1c502c2
Eliazer Braun <adardesign@gmail.com>
2020-05-19 23:34:23 -0400

    adding router and redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { combineReducers } from 'redux'
+import Layout from './layout';
 import "./styles.css";
-import Sessions from './sessions'
-import ProcedureCodes from './procedureCodes';
+
+
+const login = (state = { isLoggedIn: false }, action) => {
+  if(action.type === 'loginSuccess'){
+    return {...state, isLoggedIn: true}
+  }
+  return state;
+}
+const loading = (state = { isLoading: false }, action) => {
+  return state;
+}
+
+const rootReducer = combineReducers({
+  login,
+  loading
+})
+
+
+const store = createStore(rootReducer)
 
 
 export default function App() {
@@ -9,8 +30,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>APP</h1>
-      <Sessions />
-      <ProcedureCodes />
+      <Provider store={store}>
+        <Layout/>
+      </Provider>
     </div>
   );
 }
