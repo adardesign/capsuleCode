410d9884c84a39d88f71c6fa4b2fcd57c3f66c54
Eliazer Braun <ebraun@adorama.com>
2020-06-08 19:39:53 -0400

    custom sw

--- src/sw-build.js
+++ src/sw-build.js
@@ -1,26 +1,22 @@
-const workboxBuild = require('workbox-build');
+if (typeof importScripts === 'function') {
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+  /* global workbox */
+  if (workbox) {
+    console.log('Workbox is loaded');
+    workbox.core.skipWaiting();
 
-const buildSW = () => {
-  // The build is expected to fail if the
-  // sw install rules couldn't be generated.
-  // Add a catch block to handle this scenario.
-  return workboxBuild
-    .injectManifest({
-      swSrc: 'src/sw-custom.js', // custom sw rule
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-      swDest: 'build/sw.js', // sw output file (auto-generated
-
-      globDirectory: 'build',
-
-      globPatterns: ['**/*.{js,css,html,png,svg}'],
-
-      maximumFileSizeToCacheInBytes: 5 * 1024 * 102,
-    })
-    .then(({ count, size, warnings }) => {
-      warnings.forEach(console.warn);
-      console.info(`${count} files will be precached,
-                  totaling ${size / (1024 * 1024)} MBs.`);
-    });
-};
-
-buildSW();
+    /* custom cache rules */
+     workbox.routing.registerRoute(
+      new workbox.routing.NavigationRoute(
+        new workbox.strategies.NetworkFirst({
+          cacheName: 'PRODUCTION',
+        })
+      )
+    );
+  } else {
+    // console.log('Workbox could not be loaded. No Offline support');
+  }
+}
